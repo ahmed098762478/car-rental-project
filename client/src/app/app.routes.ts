8fe74f8efbb538ec0a1bc13187cc9fb59b23aa59
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2025-09-28 00:19:39 +0100

    website page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import {WebsiteComponent} from './components/website/website.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : 'car-rental', component: WebsiteComponent },
+    { path : 'login', component: LoginComponent },
+    { path : '', redirectTo: 'car-rental', pathMatch: 'full' },
+    { path : '**', redirectTo: 'car-rental', pathMatch: 'full' }
+  ];
---
+++ client/src/app/components/website/website.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-website',
+  imports: [RouterLink],
+  templateUrl: './website.component.html',
+  styleUrl: './website.component.css'
+})
+export class WebsiteComponent {
+
+}
